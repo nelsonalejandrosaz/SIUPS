8828147b15401a4969022e41d07b22fd93e64447
ivis12 <ivis_ccarb@hotmail.com>
2017-06-06 15:46:19 -0600

    agregando mas tablas

---
+++ database/migrations/2017_06_06_212901_create_escuelas_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateEscuelasTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('escuelas', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('codigo', 10)->unique();
+            $table->string('nombre', 25);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('escuelas');
+    }
+}
