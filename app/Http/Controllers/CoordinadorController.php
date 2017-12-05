755187cdcda8db38217492b9a20f3f02c0bdc46b
Nelson Alejandro Saz Rivera <nelsonalejandrosaz@gmail.com>
2017-12-04 22:53:45 -0600

    Inicio coordinador completo

--- app/Http/Controllers/CoordinadorController.php
+++ app/Http/Controllers/CoordinadorController.php
@@ -18,12 +18,55 @@ class CoordinadorController extends Controller
 
   public function index()
   {
+    $numeroExpNoAbierto = 0;
+    $numeroExpCurso = 0;
+    $numeroExpFinal = 0;
+    $numeroSSDisponible = 0;
+    $numeroSSEnCurso = 0;
+    $numeroSSAbandonado = 0;
+    $numeroSSFinal = 0;
   	$escuelaId = Auth::user()->escuela_id;
-	$ae = Alumno_escuela::where('escuela_id',$escuelaId)->get();
-	$numeroAlumnos = sizeof($ae);
-	$serviciosSociales = ServicioSocial::where('escuela_id',$escuelaId)->get();
-	$numeroSS = sizeof($serviciosSociales);
-	// dd($numeroAlumnos);
-    return view('home/homeCoordinador')->with(['numeroAlumnos' => $numeroAlumnos])->with(['numeroSS' => $numeroSS]);
+    $aes = Alumno_escuela::where('escuela_id',$escuelaId)->get();
+    // dd($aes);
+    foreach ($aes as $ae) {
+      if ($ae->expediente->estado_expediente->id == 1) {
+        $numeroExpNoAbierto++;
+      }
+      if ($ae->expediente->estado_expediente->id == 2) {
+        $numeroExpCurso++;
+      }
+      if ($ae->expediente->estado_expediente->id == 3) {
+        $numeroExpFinal++;
+      }
+    }
+    $serviciosSociales = ServicioSocial::where('escuela_id',$escuelaId)->get();
+    foreach ($serviciosSociales as $servicioSocial) {
+      if ($servicioSocial->estado->id == 1) {
+        $numeroSSDisponible++;
+      }
+      if ($servicioSocial->estado->id == 2) {
+        $numeroSSEnCurso++;
+      }
+      if ($servicioSocial->estado->id == 3) {
+        $numeroSSAbandonado++;
+      }
+      if ($servicioSocial->estado->id == 4) {
+        $numeroSSFinal++;
+      }
+    }
+
+    $numeroAlumnos = sizeof($aes);
+    $estadisticas = new \stdClass();
+    $estadisticas->numeroAlumnos = $numeroAlumnos;
+    $estadisticas->numeroExpNoAbierto = $numeroExpNoAbierto;
+    $estadisticas->numeroExpCurso = $numeroExpCurso;
+    $estadisticas->numeroExpFinal = $numeroExpFinal;
+    $estadisticas->numeroSSDisponible = $numeroSSDisponible;
+    $estadisticas->numeroSSEnCurso = $numeroSSEnCurso;
+    $estadisticas->numeroSSAbandonado = $numeroSSAbandonado;
+    $estadisticas->numeroSSFinal = $numeroSSFinal;
+    $estadisticas->numeroSS = sizeof($serviciosSociales);
+    // dd($estadisticas);
+    return view('home/homeCoordinador')->with(['estadisticas' => $estadisticas]);
   }
 }
